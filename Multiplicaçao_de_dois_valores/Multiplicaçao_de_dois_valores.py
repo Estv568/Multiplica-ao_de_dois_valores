9dbd7ac3e04bb963f4280db57c8888933bc66f21
Estv568 <eltororo322@gmail.com>
2023-05-06 21:39:27 -0300

    Adicionar arquivos de projeto.

---
+++ Multiplicaçao_de_dois_valores/Multiplicaçao_de_dois_valores.py
@@ -0,0 +1,7 @@
+valor1 = int(input("escreva um numero: ") )
+
+valor2 = int(input("escreva um numero: ") )
+
+resultado = valor1 * valor2
+
+new_varnew_var = print(f"O resultado de {valor1} x {valor2} e igual a {resultado} " )
